f90d3a0664f0ff13efa3de3c565c5c6fec326ed1
Geronimo <rafaelgeronimofx@gmail.com>
2024-02-21 23:20:43 -0300

    aula 02 - criando server

---
+++ backend/src/app.ts
@@ -0,0 +1,25 @@
+import express, { Application } from "express"
+import http from 'http'
+import { Server } from "socket.io"
+
+class App{
+
+  private app: Application
+  private http: http.Server
+  private io: Server
+
+  constructor() {
+    this.app = express()
+    this.http = new http.Server(this.app)
+    this.io = new Server(this.http)
+  }
+
+  public listen() {
+    this.app.listen(3333, () => {
+      console.log('Server is running on port 3333')
+    })
+  }
+
+}
+
+export { App }
